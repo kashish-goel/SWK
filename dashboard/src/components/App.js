136e5cdf413de0cff191185002818778520811d2
AnimeshN <animesh.nauti@gmail.com>
2020-11-07 00:06:39 +0530

    [Addition] React dashboard app integration

---
+++ dashboard/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <>
+      <h1>React dashboard</h1>
+      </>
+    );
+  }
+}
+
+export default App;
+
+const container = document.getElementById("app");
+render(<App />, container);
