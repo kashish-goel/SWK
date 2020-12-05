d566b069931baec4c0ea17c9e5564066ceb666d7
AnimeshN <animesh.nauti@gmail.com>
2020-12-05 18:06:16 +0530

    [addition] timeslider

--- react_dashboard/src/components/ChoroplethMap/ChoroplethMap.js
+++ react_dashboard/src/components/ChoroplethMap/ChoroplethMap.js
@@ -12,20 +12,42 @@ import {format} from 'd3-format'
 import {extent} from 'd3-array'
 import 'd3-transition'
 import L from 'leaflet';
+import { sliderLeft, sliderVertical } from 'd3-simple-slider';
+import {FormControl,InputLabel,MenuItem, Select} from '@material-ui/core'
+
+
+
+
+
+
 
 function ChoroplethMap({geojson,data,setSelLane,selCategory}) {
     const geoJsonRef = useRef();
     const dataByCategory = groupDataByCategory(data,selCategory);
+    console.log(data)
     const geojsonWithData = mergeGeomData(geojson.zones,dataByCategory);
     const domain = Object.values(dataByCategory);
     const [min,max] = extent(domain);
     const [zoneControl,setZoneControl] = useState(false);
     let colorScale= scaleSequential().domain([min,max]).interpolator(interpolateRdYlGn);
     const svgLegRef = useRef();
+    const svgSliderRef = useRef();
+
+
+
+
+    // for(var i=0; i<parentDivs.length; i++)
+    // {
+    //     var children = parentDivs[i].getElementsByClassName("svg-color-legend");
+    //     if(!children || children.length == 0)
+    //     {
+    //         parentDivs[i].style.display = "none";
+    //     }
+    // }
 
 
     // useEffect(() => {
-        // const legend = select(svgLegRef.current)
+        // const legend = select(svgSliderRef.current)
         let legend = select("#svg-color-scale")
         legend.append("g")
         .attr("class", "svg-color-legend")
@@ -44,7 +66,33 @@ function ChoroplethMap({geojson,data,setSelLane,selCategory}) {
 
     
     // }, [dataByCategory])
-    
+
+
+    // slider
+
+    var sliderVertical = sliderLeft()
+                            .min(1)
+                            .max(30)
+                            .step(1)
+                            .height(300)
+                            // .tickFormat(format('.2%'))
+                            .ticks(5)
+                            .default(0.015)
+                            .on('onchange', val => {
+                                console.log(val)
+                            // d3.select('p#value-vertical').text(d3.format('.2%')(val));
+                            });
+
+    const slider = select(svgSliderRef.current)
+                .attr('width', 100)
+                .attr('height', 350)
+                .append('g')
+                .attr('transform', 'translate(60,30)');
+
+    slider.call(sliderVertical);
+
+   
+
     const zoneStyle = (e) =>{
         return {fillColor:colorScale(e.properties.dataValue)}
     }
@@ -61,24 +109,15 @@ function ChoroplethMap({geojson,data,setSelLane,selCategory}) {
             console.log(geoJsonRef.current)
         }
 
-        // layer.bindPopup(`<b>${lane.properties.name}</b> <hr> <h1 class="text-center">${lane.properties.dataValue}</h1>`);
-        // if(lane.properties.name ==='Bhandar Vada & Amar Prem chowk'){
-        //     layer.options.style.color = "red";
-        // }
-        // if(lane.properties.name ==='HiraSethChawl - Waras Lane'){
-        //     layer.options.style.color = "yellow";
-        // }
+    
 
     layer.options.fillOpacity = .8;
 
-    // layer.options.style.color = getColor(lane.properties.dataValue,min,max,(max-min)/2);
-    // layer.options.style.color = colorScale(lane.properties.dataValue);
-    // console.log(layer.options.style.color);
-    let popup;
+    
     layer.on({
         click: e => setSelLane(e.target.feature.properties.name),
         mouseover: e => {
-            popup = L.popup()
+           L.popup()
             .setLatLng(e.latlng)
             .setContent(`<b>${e.target.feature.properties.name}</b> <hr> <h1 class="text-center" style={color:lightgreen}>${e.target.feature.properties.dataValue} kg</h1> `)
             .openOn(layer._map);
@@ -105,6 +144,7 @@ function ChoroplethMap({geojson,data,setSelLane,selCategory}) {
     let zoneControlDiv;
     if(true){
         zoneControlDiv =  <Control position="bottomleft" className="legend-div">
+
         <div className="info">
             <h4 style={{fontSize:"15px",fontWeight:"bold"}}>Click on polygon to select lane</h4>
         </div>
@@ -125,7 +165,30 @@ function ChoroplethMap({geojson,data,setSelLane,selCategory}) {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+        <Control position="bottomright">
+            <FormControl variant="outlined" >
+                <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-outlined-label"
+                        id="demo-simple-select-outlined"
+                        value={10}
+                        // onChange={handleChange}
+                        label="Age"
+                        >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+        </FormControl>
+        </Control>
+        <Control position="bottomright">
+        <svg id="svg-time-slider" ref={svgSliderRef}></svg>
+        </Control>
         {zoneControlDiv}
+
         {/* <TileLayer
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
           url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
