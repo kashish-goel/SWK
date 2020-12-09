4f22e5f004bda615f9f51e7573634ef993d96e57
AnimeshN <animesh.nauti@gmail.com>
2020-12-09 12:29:20 +0530

    [update] 13 zones

--- react_dashboard/src/components/useData.js
+++ react_dashboard/src/components/useData.js
@@ -17,7 +17,8 @@ export const FetchData = () => {
 export const FetchGeom = () =>{
     const [geom,setGeom] = useState(null);
     useEffect(()=>{
-        const urlZones = "https://gist.githubusercontent.com/AnimeshN/29199ffdba88fd7b4345ec64b54af732/raw/d1821894029a3315adad5c9c2a6dca16fe375381/dummy_lane_polygon_v1.1.geojson";
+        const urlZones = "https://makerghat.urbansciences.in/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typename=geonode%3Aswk_13_zone_polygons&outputFormat=json&srs=EPSG%3A4326&srsName=EPSG%3A4326";
+        // const urlZones = "https://gist.githubusercontent.com/AnimeshN/29199ffdba88fd7b4345ec64b54af732/raw/d1821894029a3315adad5c9c2a6dca16fe375381/dummy_lane_polygon_v1.1.geojson";
         const urlSpots = "https://makerghat.urbansciences.in/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typename=geonode%3Aspots_3december&outputFormat=json&srs=EPSG%3A4326&srsName=EPSG%3A4326&access_token=ajmGSRmLlZVYgYNbYJ486nO8BUZkbo";
         json(urlZones,zones=>{
             json(urlSpots,spots =>{
